24bc680d3ae102884815aafdf6469f8412150657
Praise1214 <agapepraiz@gmail.com>
2026-02-08 09:00:12 +0100

    chore: initialize app scaffold with Prisma 7 config and Express server

---
+++ src/config/db.ts
@@ -0,0 +1,29 @@
+import { PrismaClient } from "@prisma/client";
+
+
+const prisma = new PrismaClient ({
+  log:
+  process.env.NODE_ENV === "development"
+    ? ["query", "error", "warn"]
+    :["error"],
+})
+
+const connectDB = async () => {
+  try{
+    await prisma.$connect();
+    console.log("DB connected via prisma")
+  }
+  catch (error) {
+    if(error instanceof Error) {
+      console.error(`Database connection error: ${error.message}`)
+    }else {
+      console.error(`Database connection error:`, error)
+    }
+  }
+}
+
+const disconnectDB = async () => {
+  await prisma.$disconnect();
+}
+
+export {prisma, connectDB, disconnectDB}
---
+++ src/server.ts
@@ -0,0 +1,49 @@
+import express from "express";
+import { config } from "dotenv";
+import { connectDB, disconnectDB } from "./config/db";
+
+import movieRoutes from "./routes/movieRoutes";
+
+config();
+connectDB();
+
+const app = express();
+app.use("/movies", movieRoutes)
+
+
+app.get("/hello", (req,res) => {
+  res.json({message: "Hello World"})
+})
+
+const PORT = 5001;
+
+const server = app.listen(PORT, () => {
+  console.log(`server is running on ${PORT}`)
+})
+
+//Handle unhandled promise rejections
+process.on("unhandledRejection", (err) => {
+  console.error("Unhandled Rejection:", err);
+  server.close(async () => {
+    await disconnectDB();
+    process.exit(1)
+  } )
+})
+
+//Handle uncaught expressions
+process.on("uncaughtException", (err) => {
+  console.error("Uncaught Exception", err);
+  server.close(async () => {
+    await disconnectDB();
+    process.exit(1)
+  })
+})
+
+//Graceful Shutdown
+process.on("SIGTERM", (err) => {
+  console.error("SIGTERM recieved, shutting down gracefully")
+  server.close(async () => {
+    await disconnectDB();
+    process.exit(0);
+  })
+})
